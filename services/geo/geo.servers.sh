910476f6c6d804c21e49d83d8368ff54b6debd94
alex <alexey.egoshin12@gmail.com>
2015-10-26 12:33:28 +0300

    russia.sqlite

--- services/geo/geo.servers.sh
+++ services/geo/geo.servers.sh
@@ -4,4 +4,4 @@
 #node services/geo/geo.server.js 8012 vietnam-latest.osm.sqlite &
 #node services/geo/geo.server.js 8014 ukraine-latest.osm.sqlite &
 #node services/geo/geo.server.js 8013 RU-MOS.osm.sqlite &
-node services/geo/geo.server.js 8015 texas-latest.osm.sqlite
+node services/geo/geo.server.js 8015 russia.osm.sqlite
